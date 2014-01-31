03b3d103959a88ab688bb3929e250702453d72e7
Jonathan Gamba <jonathan.gamba@dotcms.com>
2014-01-31 15:38:51 -0600

    Fixed problems on windows

--- bin/dotShutdown.bat
+++ bin/dotShutdown.bat
@@ -1,8 +1,10 @@
 @echo off
 
 rem -----------------------------------------------------------------------------
-rem Start Script for the dotCMS Server
+rem Stop Script for the dotCMS Server
 rem -----------------------------------------------------------------------------
+
+if "%OS%" == "Windows_NT" setlocal
 
 @if not "%ECHO%" == ""  echo %ECHO%
 @if "%OS%" == "Windows_NT" setlocal
@@ -50,24 +52,14 @@ echo This environment variable is needed to run this program
 goto end
 :okHome
 
-rem Java VM configuration options
-
-if not "%JAVA_OPTS%" == "" goto noDefaultJavaOpts
-set JAVA_OPTS=-Djava.awt.headless=true -Xverify:none -Dfile.encoding=UTF8 -server -Xmx1G -Djava.endorsed.dirs=%DOTCMS_HOME%/WEB-INF/endorsed_libs  -XX:MaxPermSize=256m -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -javaagent:%DOTCMS_HOME%/WEB-INF/lib/jamm-0.2.5.jar
-rem Uncomment the next line if you want to enable JMX
-rem set JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.port=7788 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.endorsed.dirs=$DOTCMS_HOME/WEB-INF/endorsed_libs
-:noDefaultJavaOpts
-
 set "EXECUTABLE=%CATALINA_HOME%\bin\catalina.bat"
 
 rem Check that target executable exists
-
 if exist "%EXECUTABLE%" goto okExec
 echo Cannot find "%EXECUTABLE%"
 echo This file is needed to run this program
 goto end
 :okExec
-
 
 rem Get remaining unshifted command line arguments and save them in the
 set CMD_LINE_ARGS=
@@ -78,9 +70,8 @@ shift
 goto setArgs
 :doneSetArgs
 
-rem Executing Tomcat
 cd %CATALINA_HOME%\bin
-call "%EXECUTABLE%" start %CMD_LINE_ARGS%
+call "%EXECUTABLE%" stop %CMD_LINE_ARGS%
 cd "%CURRENT_DIR%"
 
 :end
